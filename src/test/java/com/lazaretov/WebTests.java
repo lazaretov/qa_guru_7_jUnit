f7befe1757ee2605bf66efd35686ab20f59edbf4
Timofei <lazaretov03@gmail.com>
2022-10-25 14:13:31 +0600

    add EnumSource ParameterizedTest

--- src/test/java/com/lazaretov/WebTests.java
+++ src/test/java/com/lazaretov/WebTests.java
@@ -5,14 +5,12 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.conditions.CssClass;
+import com.lazaretov.testData.ButtonsNames;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -25,7 +23,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class WebTests {
 
     @BeforeAll
-    static void configure(){
+    static void configure() {
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
     }
@@ -33,21 +31,21 @@ public class WebTests {
     @Disabled
     @Test
     void vcSearchTest() {
-        open ("https://vc.ru");
+        open("https://vc.ru");
         $(".v-text-input__input").setValue("Илон Маск").pressEnter();
         $$(".content-container").first().shouldHave(text("Илон Маск"));
     }
 
-    @Disabled
+
     @ValueSource(strings = {"Илон Маск", "Ozon"})
     @ParameterizedTest(name = "Соответствие поисковых результатов для запроса {0}")
     void vcParamSearchTest(String testData) {
-        open ("https://vc.ru");
+        open("https://vc.ru");
         $(".v-text-input__input").setValue(testData).pressEnter();
         $$(".content-container").first().shouldHave(text(testData));
     }
 
-    @Disabled
+
     @CsvSource(value = {
             "Илон Маск | должен будет оплатить $1 млрд, если сделка не состоится. В мае",
             "Ozon | оказалось, в Астрахани есть несколько производств: завод резиновой обуви"},
@@ -55,7 +53,7 @@ public class WebTests {
     )
     @ParameterizedTest(name = "Соответствие поисковых результатов для запроса {0}")
     void vcParamSearchResultTest(String searchQuery, String expectedText) {
-        open ("https://vc.ru");
+        open("https://vc.ru");
         $(".v-text-input__input").setValue(searchQuery).pressEnter();
         $$(".content-container").first()
                 .shouldHave(text(searchQuery))
@@ -73,23 +71,24 @@ public class WebTests {
                 Arguments.of(List.of("Популярное", "Свежее", "Компании", "Вакансии", "Мероприятия", "Подписки", "Личный опыт", "Маркетинг", "Дизайн", "Техника", "Карьера"), "https://vc.ru/subs")
         );
     }
+
     @MethodSource("vcParamButtonText")
     @ParameterizedTest(name = "Наличие кнопок меню на странице {1}")
     void vcParamButtonText(List<String> buttonName, String Link) {
-        open (Link);
+        open(Link);
         $$(".sidebar-tree-list-item a").filter(visible)
                 .excludeWith(text("Где реклама?"))
                 .shouldHave(CollectionCondition.texts(buttonName));
     }
 
-
-
-
-
-
-
-
-
+    @EnumSource(ButtonsNames.class)
+    @ParameterizedTest(name = "Видимость кнопки меню {0} на главное странице")
+    void vcParamButtonVisibleTest(ButtonsNames buttonName) {
+        open("https://vc.ru");
+        $$(".sidebar-tree-list-item a")
+                .find(text(buttonName.getButtonName()))
+                .shouldBe(visible);
+    }
 
 
 }
